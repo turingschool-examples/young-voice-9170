51676974cfa4dee9635e986099e5129e02e9dda6
Austin Moore <austinchristianmoore@gmail.com>
2022-02-12 10:13:54 -0500

    Feature: Set up db relationships/validations

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,3 +1,7 @@
 class Professor <ApplicationRecord
-
+  has_many :professor_students
+  has_many :students, through: :professor_students
+  validates_presence_of :name
+  validates_presence_of :age
+  validates_presence_of :specialty
 end
