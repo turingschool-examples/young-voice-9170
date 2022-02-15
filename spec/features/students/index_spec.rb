cfffa5b96cef12ce44834c3943bef75ed5b4c2ca
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-15 15:05:57 -0700

    Fix tabs and spacing

--- spec/features/students/index_spec.rb
+++ spec/features/students/index_spec.rb
@@ -3,21 +3,21 @@ require 'rails_helper'
 RSpec.describe 'students index page' do
   
   before(:each) do
-     @snape = Professor.create!(name: "Severus Snape", age: 45, specialty: "Potions")
-        @hagrid = Professor.create!(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
-        @lupin = Professor.create!(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+    @snape = Professor.create!(name: "Severus Snape", age: 45, specialty: "Potions")
+    @hagrid = Professor.create!(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+    @lupin = Professor.create!(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
 
-        @malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
-        @harry = Student.create!(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
-        @longbottom = Student.create!(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
-      
-        ProfessorStudent.create(student_id: @harry.id, professor_id: @snape.id)
-        ProfessorStudent.create(student_id: @harry.id, professor_id: @hagrid.id)
-        ProfessorStudent.create(student_id: @harry.id, professor_id: @lupin.id)
-        ProfessorStudent.create(student_id: @malfoy.id, professor_id: @hagrid.id)
-        ProfessorStudent.create(student_id: @malfoy.id, professor_id: @lupin.id)
-        ProfessorStudent.create(student_id: @longbottom.id, professor_id: @snape.id)
-    
+    @malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+    @harry = Student.create!(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+    @longbottom = Student.create!(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+  
+    ProfessorStudent.create(student_id: @harry.id, professor_id: @snape.id)
+    ProfessorStudent.create(student_id: @harry.id, professor_id: @hagrid.id)
+    ProfessorStudent.create(student_id: @harry.id, professor_id: @lupin.id)
+    ProfessorStudent.create(student_id: @malfoy.id, professor_id: @hagrid.id)
+    ProfessorStudent.create(student_id: @malfoy.id, professor_id: @lupin.id)
+    ProfessorStudent.create(student_id: @longbottom.id, professor_id: @snape.id)
+
     visit '/students'
   end
 
