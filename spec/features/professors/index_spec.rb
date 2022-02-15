f417a7dfc6767a1f69a8719524d8cdbf4ed0b3be
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-15 14:18:09 -0700

    Change test data

--- spec/features/professors/index_spec.rb
+++ spec/features/professors/index_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'professors index page' do
     
     @flitwick = Professor.create!(name: "Filius Flitwick", age: 230, specialty: "Charms")
     @umbridge = Professor.create!(name: "Dolores Umbridge", age: 150, specialty: "Defence Against the Dark Arts")
-    @longbottom = Professor.create!(name: "Neville Longbottom", age: 60, specialty: "Herbology")
+    @lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
     
     visit '/professors'
   end 
@@ -15,6 +15,6 @@ RSpec.describe 'professors index page' do
 
     expect(page).to have_content(@flitwick.name)
     expect(page).to have_content(@umbridge.name)
-    expect(page).to have_content(@longbottom.name)
+    expect(page).to have_content(@lupin.name)
   end
 end
