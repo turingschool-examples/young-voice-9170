fbacd8b1b9fb1b1c924d324ae35a82f3f5546749
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-15 13:37:45 -0700

    Add validations for attributes

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,9 @@
 class Student <ApplicationRecord
   has_many :professor_students
   has_many :professors, through: :professor_students
+
+  validates_presence_of :name
+  validates_presence_of :age
+  validates_presence_of :house
+
 end
