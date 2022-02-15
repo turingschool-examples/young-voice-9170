0bb19284d2a0e7918aedb2af11b5162f6da77951
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-15 14:18:54 -0700

    Add show method

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -3,4 +3,9 @@ class ProfessorsController < ApplicationController
   def index
     @professors = Professor.all
   end
+
+  def show
+    @professor = Professor.find(params[:id])
+    @students = @professor.students
+  end
 end
