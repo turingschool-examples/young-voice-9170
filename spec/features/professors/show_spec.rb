13af83b5d5545b35fefc62addb41bea9c3316d4d
Jessica Grazulis <jlgrazulis@gmail.com>
2022-02-15 15:29:06 -0700

    Add feature test

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -3,12 +3,15 @@ require 'rails_helper'
 RSpec.describe 'professor show page' do
 
   before(:each) do
-    @snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
-    @lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+    @snape = Professor.create!(name: "Severus Snape", age: 40, specialty: "Potions")
+    @lupin = Professor.create!(name: "Remus Lupin", age: 50 , specialty: "Defense Against The Dark Arts")
 
-    @harry = @snape.students.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
-    @longbottom = @snape.students.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
-    @malfoy = @lupin.students.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+    @harry = Student.create!(name: "Harry Potter" , age: 15 , house: "Gryffindor" )
+    @longbottom = Student.create!(name: "Neville Longbottom" , age: 10 , house: "Gryffindor" )
+    @malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+
+    ProfessorStudent.create(student_id: @harry.id, professor_id: @snape.id)
+    ProfessorStudent.create(student_id: @longbottom.id, professor_id: @snape.id)
     
     visit "/professors/#{@snape.id}"
   end
@@ -18,4 +21,9 @@ RSpec.describe 'professor show page' do
     expect(page).to have_content(@longbottom.name)
     expect(page).to_not have_content(@malfoy.name)
   end
+
+  scenario 'visitor sees average age of all students for professor' do
+    expect(page).to have_content(@snape.avg_student_age)
+  end
+
 end
